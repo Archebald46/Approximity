4ddf7050bfa14cbe89ecabef811da92e801d9488
Arthur <senior.kretoff@yandex.ru>
2015-04-19 09:22:56 +0300

    Comments and fix

--- app/src/main/java/com/example/approx/MoveSense.java
+++ app/src/main/java/com/example/approx/MoveSense.java
@@ -6,12 +6,9 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.CountDownTimer;
-
 import android.widget.ProgressBar;
 
-
-
-public class MoveSense {
+    public class MoveSense {  // класс, который отвечает за обработку всех движений по акселерометру
     SensorManager sensorManager;
     Sensor sensorAccel;
     private float mAccel;
@@ -19,7 +16,6 @@ public class MoveSense {
     private float mAccelLast,sum;
     private Context context;
     public static int count = 0;
-
     private int strong;
     public float x, y, z;
     private int actionCode,dex;
@@ -27,80 +23,83 @@ public class MoveSense {
     int time,strnI, progress;
     double strn;
     ProgressBar PB;
-
     CountDownTimer countDownTimer;
-    public MoveSense(Context ctx,int str, int code, int dx, ProgressBar progressBar){
+
+    public MoveSense(Context ctx,int str, int code, int dx, ProgressBar progressBar){ // получаем входные данные, переданные из main
         PB = progressBar;
         strong=str;
         dex = dx;
-        strn = 16 - ((strong -1)* 0.061);
-        strnI = (int) Math.round(strn);
-        time = 2000 + 61*(dex - 1);
+        strn = 16 - ((strong -1)* 0.162); // вычисление количества встрясок, зависящего от силы персонажа
+        strnI = (int) Math.round(strn); // округление до целого
+        time = 2000 + 61*(dex - 1); // вычисление времени,отведенного на выполнение жесты, в зависимости от ловкости персонажа
         actionCode=code;//1-тряска 2-бросок
-        sensorManager = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
+
+        sensorManager = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE); // регистрация и подключение к акселерометру
         sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
         mAccel = 0.00f;
         context=ctx;
         progress = (int) Math.round(100/(strnI+0.3));
         mAccelCurrent = SensorManager.GRAVITY_EARTH;
         mAccelLast = SensorManager.GRAVITY_EARTH;
-        sensorManager.registerListener(listener, sensorAccel, SensorManager.SENSOR_DELAY_UI);  // это,короче, слушатели и время опроса датчиков
-        countDownTimer = new CountDownTimer(time,500) {
+        sensorManager.registerListener(listener, sensorAccel, SensorManager.SENSOR_DELAY_UI);  // это,короче, слушатели и время опроса акселерометра
+        countDownTimer = new CountDownTimer(time,500) {  // запускаем таймер, которые отсчитывает время, данное на выполнение жеста
             @Override
             public void onTick(long millisUntilFinished) {
             }
             @Override
             public void onFinish() {
-                if (act == 0) {
+                if (act == 0) { // если действие не было выполнено
                     mAccel=0;
                     count=0;
-                    main.setActionInt(actionCode,act);
+                    main.setActionInt(actionCode,act); // отправляем данные в main о том, что действие провалено
                     //main.setProgressB(0);
-                    PB.setProgress(0);
+                    PB.setProgress(0); // обнуляем прогрессбар
                     act = 0;
-                    isUnregistred();
+                    isUnregistred();  // разрегистрируем акселерометр
                 }
 
             }
         };
         countDownTimer.start();
     }
-    SensorEventListener listener = new SensorEventListener() {
+    SensorEventListener listener = new SensorEventListener() {  //слушатель для самого акселерометра
         @Override
         public void onSensorChanged(SensorEvent event) {
-            switch(actionCode){
-                case 1://тряска
+            switch(actionCode){ //выполнение действия в зависимости от выбранного радиобатона
+                case 1: //тряска
                     z = event.values[2];
                     mAccelLast = mAccelCurrent;
                     mAccelCurrent=z;
-                    float delta = mAccelCurrent - mAccelLast;
+                    float delta = mAccelCurrent - mAccelLast;  // суть всего: расчет разницы между начальным ускороение и конечным по оси Z при тряске
                     mAccel = mAccel * 0.9f + delta;
-                    if (mAccel >12) { //вместо стронг стоит число 12, то есть сила конкретная
-                        count++;
+                    if (mAccel >8) { //вместо стронг стоит число 8, то есть сила конкретная; если разница > 8, то срабатывает счетчик
+                        count++; // отсчитывает требуемое количество встрясок
                         //main.setProgressB(progress);
+
                         if (PB.getProgress()<100){
-                            PB.incrementProgressBy(progress);}else{
+                            PB.incrementProgressBy(progress);}else{ // отвечает за заполнение прогрессбара в процессе тряски
                             PB.setProgress(100);}
                     }
-                    if (count == strnI) {
+                    if (count == strnI) { //если треуемое число встрясок достигнуто, то останавливаем сенсор
                         act = 1;
                         mAccel=0;
                         count=0;
-                        main.setActionInt(actionCode,act);
+                        main.setActionInt(actionCode,act); // передаем данные в main об успешном выполнении
                         act = 0;
                         isUnregistred();
                     }
                     break;
-                case 2://бросок
+                case 2: //бросок
                     x = event.values[0];
                     y = event.values[1];
                     z = event.values[2];
-                    sum=x+y+z;
-                    if (sum>10*3||sum<-12*3){ //вместо стронг стоит число 12, то есть сила конкретная
+                    sum=x+y+z; //собираем данные об ускорении со всех осей и вычиссляем их сумму
+                    if (sum>8*3||sum<-8*3){ //вместо стронг стоит число 8, то есть сила конкретная; если эта сумма больше заданного чила, то действие выполнено
                         act = 2;
                         mAccel=0;
                         count=0;
-                        main.setActionInt(actionCode,act);
+                        main.setActionInt(actionCode,act); // передаем данные в main об успешном выполнении дейтвия
                         act = 0;
                         isUnregistred();
                 }
@@ -116,12 +115,13 @@ public class MoveSense {
 
 
 
-    public boolean isUnregistred(){
+    public boolean isUnregistred(){ // разрегистрация акселерометра
         if (sensorManager != null){
         sensorManager.unregisterListener(listener);}
         if (sensorAccel !=null){
         sensorAccel=null;}
-        if (countDownTimer !=null){
+
+        if (countDownTimer !=null){ //обнуление таймера
         countDownTimer.cancel();
         countDownTimer = null;}
         count = 0;
@@ -129,7 +129,7 @@ public class MoveSense {
     }
 
 
-    public static int getCount() {
+    public static int getCount() { // метод для получения количества сделанных встрясок в любой момент
 
         return count;
     }
--- app/src/main/java/com/example/approx/main.java
+++ app/src/main/java/com/example/approx/main.java
@@ -22,6 +22,7 @@ import static android.widget.RadioGroup.OnCheckedChangeListener;
 
 
 public class main extends Activity implements View.OnClickListener, OnCheckedChangeListener {
+
     Button str;
     Button gt;
     Button cln;
@@ -41,27 +42,32 @@ public class main extends Activity implements View.OnClickListener, OnCheckedCha
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,   //установка полноэкранного режима для активити
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
         str = (Button) findViewById(R.id.button_start);
         gt = (Button) findViewById(R.id.button_get);
         cln = (Button) findViewById(R.id.button_clean);
-        rShake = (RadioButton) findViewById(R.id.radioShake);
+        rShake = (RadioButton) findViewById(R.id.radioShake);     //регистрация используемых компонентов (кнопок и прочего)
         rDrop = (RadioButton) findViewById(R.id.radioDrop);
         rg = (RadioGroup) findViewById(R.id.RG);
         dexter = (EditText) findViewById(R.id.dexterText);
         strength = (EditText) findViewById(R.id.strText);
         progressB = (ProgressBar) findViewById(R.id.progressBar);
         toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_LONG);
-        str.setOnClickListener(this);
+
+        str.setOnClickListener(this); // установка слушателя на кнопки для обработки нажатий на них
         gt.setOnClickListener(this);
         cln.setOnClickListener(this);
-        rg.setOnCheckedChangeListener(this);
+
+        rg.setOnCheckedChangeListener(this); // слушатель для радиогруппы, в которой лежат радиобатоны
     }
 
- public static void setActionInt (int first, int second){
+ public static void setActionInt (int first, int second){   //получает данные из класса moveSense по завершении дейтвия, чтобы вывести определенный результат
 
      if (first ==1 && second==1){
            toast.setText("Тряска ОК! Действие: "+ first +" Стастус "+ second);
@@ -92,7 +98,7 @@ public class main extends Activity implements View.OnClickListener, OnCheckedCha
 
     /*public static void setProgressB (int prog) {
         if (progressB.getProgress()<100){
-            progressB.incrementProgressBy(prog);}else{
+            progressB.incrementProgressBy(prog);}else{   //это еще один вариант юзания прогрессбара, но его не трогаем
             progressB.setProgress(100);
         }
     }*/
@@ -106,7 +112,7 @@ public class main extends Activity implements View.OnClickListener, OnCheckedCha
     @Override
     protected void onPause() {
         if (moveSense != null){
-        moveSense.isUnregistred();}
+        moveSense.isUnregistred();} //снятие регистрации с акселерометра, дабы он не работал, когда не нужно
 
         super.onPause();
     }
@@ -114,21 +120,19 @@ public class main extends Activity implements View.OnClickListener, OnCheckedCha
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button_start:
+            case R.id.button_start:  // кнопка Старт
                 if (rShake.isChecked() || rDrop.isChecked()) {
-                    if (dexter.getText().toString().equals("")||strength.getText().toString().equals("")){
+                    if (dexter.getText().toString().equals("")||strength.getText().toString().equals("")){ //если все заполнено и выбрано, то запускем сенсор
                         toast.setText("Введи силу/ловкость от 1 до 100");
                         toast.show();
                         break;
                     } else{
-                    dexSTR = Integer.parseInt(dexter.getText().toString());
+                    dexSTR = Integer.parseInt(dexter.getText().toString());  // получаем значения из эдитов
                     strSTR = Integer.parseInt(strength.getText().toString());
                     if (moveSense !=null){
                         moveSense.isUnregistred();}
-                        progressB.setProgress(0);
-                    moveSense = new MoveSense(this, strSTR, radiobtn, dexSTR, progressB);
-
-
+                        progressB.setProgress(0); // обнуляем прогрессбар при запуске
+                    moveSense = new MoveSense(this, strSTR, radiobtn, dexSTR, progressB); //передаем входные параметры (сила,действие,ловкость и прогрессбар) в класс moveSense для старта
                      break;}
                 } else {
                     toast.setText("Не выбрано действие");
@@ -137,17 +141,16 @@ public class main extends Activity implements View.OnClickListener, OnCheckedCha
                 }
 
 
-            case R.id.button_get:
-
+            case R.id.button_get: //кнопка Кол-во встрясок
                 i = moveSense.getCount();
-                toast.setText("Кол-во встрясок: " + i);
+                toast.setText("Кол-во встрясок: " + i); // получаем количество встрясок телефона при действии "Тряска"
                 toast.show();
                 break;
 
-            case R.id.button_clean:
+            case R.id.button_clean: //кнопка Clean
                 rg.clearCheck();
                 if (moveSense !=null){
-                moveSense.isUnregistred();}
+                moveSense.isUnregistred();} // сбрасывает выбор радиобатонов, обнуляет прогрессбар
                 radiobtn = 0;
                 progressB.setProgress(0);
                 break;
@@ -156,18 +159,18 @@ public class main extends Activity implements View.OnClickListener, OnCheckedCha
     }
 
     @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
+    public void onCheckedChanged(RadioGroup group, int checkedId) { // следит за изменениями в радиогруппе, то есть, за выбранным радиобатоном
         // TODO Auto-generated method stub
         switch (checkedId) {
             case R.id.radioDrop:
                 if (moveSense !=null){
-                moveSense.isUnregistred();}
+                moveSense.isUnregistred();} //передает 2, если выбран Бросок
                 radiobtn = 2;
                 break;
 
             case R.id.radioShake:
                 if (moveSense !=null){
-                moveSense.isUnregistred();}
+                moveSense.isUnregistred();} // передает 1, если выбрана Тряска
                 radiobtn = 1;
                 break;
 
